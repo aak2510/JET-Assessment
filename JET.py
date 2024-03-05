d3ab7fd34146ff512e30e25115e9b806322148d3
aak2510 <akanda2500@gmail.com>
2024-03-05 19:30:43 +0000

    Consuming API to display the requested data
    Created a cuisines list to store the cuisine of each restaurant in the restaurant object before display data for each restaurant in a formatted way.
    
    Displaying the format of each restaurant with the required information: name cuisines, rating, address

--- JET.py
+++ JET.py
@@ -1,6 +1,6 @@
 import requests
 
-postcode = "UB56LH" 
+postcode = "UB56LH"
 
 url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
 headers = {
@@ -11,9 +11,30 @@ response = requests.get(url, headers=headers)
 
 
 if response.status_code == 200:
-    
+
     data = response.json()["restaurants"][:10]
 else:
     print(response.status_code)
 
-print(data)
+if response.status_code == 200:
+    data = response.json()["restaurants"][:10]
+else:
+    print(response.status_code)
+
+
+for restaurant in data:
+    print("Name: " + str(restaurant["name"]))
+    print("restaurant rating: " + str(restaurant["rating"]["starRating"]))
+    # For each restuarent we need to create a list of its cuisines to print on one line
+    cuisineList = []
+    for cuisine in restaurant["cuisines"]:
+        cuisineList.append(cuisine["name"])
+    print(f"cuisine: {', '.join(cuisineList)}")
+    print(
+        "address: {}, {}, {}".format(
+            restaurant["address"]["firstLine"],
+            restaurant["address"]["city"],
+            restaurant["address"]["postalCode"],
+        )
+    )
+    print()
