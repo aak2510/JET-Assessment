66a65351643af044612a67e485b47c74c94c3ffc
aak2510 <akanda2500@gmail.com>
2024-03-05 20:17:48 +0000

    Re-integrate new table solution into methods
    All data received from the API call will now be displayed in a table format using tabulate.
    
    Re integrated all the table code into the corresponding functions that were initially implemented to display data as a list.
    
    TODO/FIX
    - Implement functionality to ensure error response codes are dealt with e.g. if the server returns a 403 error code and, in some cases, the sever return empty data which need to be handled responsibly.
    
    - Add comments in the code
    
    - Make efficient where possible
    
    - Implement feature to allow users to input a post code of their choice

--- JET.py
+++ JET.py
@@ -1,89 +1,53 @@
 import requests
 from tabulate import tabulate
 
-# def get_restaurant_data(postcode):
 
-#     url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
-#     headers = {
-#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-#     }
+def call_api(postcode):
 
-#     response = requests.get(url, headers=headers)
-
-#     if response.status_code == 200:
-
-#         data = response.json()["restaurants"][:10]
-#         return data
-#     else:
-#         print(response.status_code)
-
-
-# def display_restaurants(data):
-
-#     for restaurant in data:
-#         print("Name: " + str(restaurant["name"]))
-#         print("restaurant rating: " + str(restaurant["rating"]["starRating"]))
-#         # For each restuarent we need to create a list of its cuisines to print on one line
-#         cuisineList = []
-#         for cuisine in restaurant["cuisines"]:
-#             cuisineList.append(cuisine["name"])
-#         print(f"cuisine: {', '.join(cuisineList)}")
-#         print(
-#             "address: {}, {}, {}".format(
-#                 restaurant["address"]["firstLine"],
-#                 restaurant["address"]["city"],
-#                 restaurant["address"]["postalCode"],
-#             )
-#         )
-#         print()
-
-
-# def main():
-#     # Replace with your desired postcode
-#     postcode = "UB56LH"
-
-#     restaurants = get_restaurant_data(postcode)
-
-#     if restaurants:
-#         display_restaurants(restaurants)
-#     else:
-#         print("Failed to fetch restaurant data.")
-
-
-# if __name__ == "__main__":
-#     main()
-
-postcode = "UB56LH"  # input("Please enter your postcode: ").replace(" ", "").upper()
-
-url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-}
-
-response = requests.get(url, headers=headers)
-# print(response.headers)
-
-
-if response.status_code == 200:
-    data = response.json()["restaurants"][:10]
-else:
-    print("Something went wrong... {}".format(response.status_code))
+    url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{postcode}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()["restaurants"][:10]
+    else:
+        print("Something went wrong... {}".format(response.status_code))
 
 
 
-tableData = []
-for restaurant in data:
-    cuisineList = []
-    for cuisine in restaurant["cuisines"]:
-        cuisineList.append(cuisine["name"])
+    
 
-    tableData.append([
-            restaurant["name"],
-            "\n".join(cuisineList),
-            restaurant["rating"]["starRating"],
-            f"{restaurant["address"]["firstLine"]}\n{restaurant["address"]["city"]}\n{restaurant["address"]["postalCode"]}"
-            ])
 
-headers = ["Restaurant name", "Cuisines", "rating", "Address"]
+def format_response_data(data):
 
-print(tabulate(tableData, headers, tablefmt="grid", numalign="center", stralign="center"))
+    tableData = []
+    for restaurant in data:
+        cuisineList = []
+        for cuisine in restaurant["cuisines"]:
+            cuisineList.append(cuisine["name"])
+
+        tableData.append([
+                restaurant["name"],
+                "\n".join(cuisineList),
+                restaurant["rating"]["starRating"],
+                f"{restaurant["address"]["firstLine"]}\n{restaurant["address"]["city"]}\n{restaurant["address"]["postalCode"]}"
+                ])
+
+
+    return tableData
+
+def print_table(tableData):
+
+    headers = ["Restaurant name", "Cuisines", "rating", "Address"]
+    print(tabulate(tableData, headers, tablefmt="grid", numalign="center", stralign="center"))
+
+
+def main():
+    print_table(format_response_data(call_api("UB56LH")))
+
+    
+
+if __name__ == "__main__":
+    main()
+
