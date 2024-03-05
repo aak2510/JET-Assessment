f3a39c8e653629064d7b9791478b6f10596bb569
aak2510 <akanda2500@gmail.com>
2024-03-05 21:06:56 +0000

    Added user input functionality for postcode and updated comments
    Added a "menu" option/user input functionality where the user can choose any postcode they want to enter and find the restaurants for that location. If the user wants to exit the program, they can do so with a graceful exit using the 'q' button.
    
    Updated comments to make them more readable where needed
    
    Added help string for each function.

--- JET.py
+++ JET.py
@@ -17,35 +17,35 @@ def call_api(postcode):
     if response.status_code == 200:
         # Serialise to json after ensuring a successful call
         data = response.json()
-        # if there is a successful response (HTTP code 200), we can then check to make sure the server hasn't sent back empty data (which is the case with invalid postcode input)
-        # This can be checked by looking at the resultCount value withint he servers response
+        # Server can still respond with a "200" code if given an invalid postcode by returning an empty JSON object as a "failed" response 
+        # So we check to see if there are restaurants to return 
         if data['metaData']['resultCount'] > 0:
-            # if we have data to return, we return the first 10 restaurants
+            # if there are restaurants to return then we only return the first 10 restaurant objects
             return data["restaurants"][:10]
         else:
-            # if there is not restaurant returned, it means we entered the postcode incorrectly or there is nothing being served in this area. 
+            # otherwise if we do get an empty JSON object i.e. no restaurants, we tell the user there are no restaurants in that given postcode area
             print("There are no Restaurants currently serving within this postcode.\n\n")
             return
     # (ERROR handling) If the call to the API was unsuccessful, we return/print information about that error 
     else:
         try:
-           # If the response was able to serialise but the HTTP status code indicates an error, then we need to handle it
-            # API endpoint/Server responds with Json object for some erroneous status' such as 401, 429, 500.
-            # If it does, this contains an error code and description we will try to access and print.
+           # API endpoint/Server responds with Json object for some erroneous status' such as 401, 429, 500. If it does, this contains an error code and description we will try to access and print.
             errorResponse = response.json()["errors"]
             print(f"Error code: {errorResponse["errorCode"]}.\nReason: {errorResponse["description"]}.\n\n")
         except:
-           # If the json serialisation fails, which can happen if there is, for example, a 402 error with no content, 
-        # then we can return the error code and reason using the requests library
+            # If the API endpoint/Server doesn't or can't return an Object for a failed call, then the json() function will throw an exception which we will catch. 
+            # We then print the requests library's HTTP error code and reason instead of the endpoint's. 
             print(f"Error code: {response.status_code}.\nReason: {response.reason}.\n\n")
 
 
-def format_response_data(data):
 
-    # store each row of the table in a list as a list of lists
+def format_response_data(data):
+    """Formatting the response data so that it can be displayed properly in each row of the table."""
+
+    # store each data row of the table in a list, as a list of lists
     tableData = []
     for restaurant in data:
-        # Each restaurant can have multiple cuisines, so we store these in another list
+        # Each restaurant can have multiple cuisines, so we store these in another temporary list so that we can correctly formulate and print the table
         cuisineList = []
         for cuisine in restaurant["cuisines"]:
             cuisineList.append(cuisine["name"])
@@ -63,19 +63,32 @@ def format_response_data(data):
     return tableData
 
 def print_table(tableData):
+    """Printing the table with headers and row data. Headers are the requested data points, while row data is the response from the API."""
+
     # Requested data points to display. These will be the headings of the table
     headers = ["Restaurant name", "Cuisines", "rating", "Address"]
-    print(tabulate(tableData, headers, tablefmt="grid", numalign="center", stralign="center"))
+    print(tabulate(tableData, headers, tablefmt="grid", numalign="center", stralign="center") + "\n")
 
 
 def main():
-    # if the wrong postcode is entered, then data is still returned by the API but its empty data, so we need to handle that
-    # we do that by checking if data is no null
-    data = call_api("UB56LH")
-    if data:
-        print_table(format_response_data(data))
 
-    
+    # run function until user wants to quit
+    while True:
+       
+        # Ask the user to input a postcode, this is formatted to remove all types of spaces including tabs and newlines, then converted to upper for use in the API
+        userInput = "".join(input("Please enter your postcode or \"q\" to quit: ").split()).upper()
+        # If the user inputs a single 'q' they quit the program
+        if userInput == 'Q':
+            break
+        
+        # if the wrong postcode is entered, then data is still returned by the API but its empty data, so we need to handle that
+        # we do that by checking if data is no null
+        data = call_api(userInput)
+        if data:
+            print_table(format_response_data(data))
+
+
+
 
 if __name__ == "__main__":
     main()
